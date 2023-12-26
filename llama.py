d6b8a5e2a8e7f626170cf5d39384a3756caa9b99
Jesse-0x <jgong@whschool.org>
2023-12-26 16:50:35 +0800

    Finish evaluating the model

--- llama.py
+++ llama.py
@@ -7,7 +7,8 @@ llm = Llama(model_path="/Users/jesse/Doc/github/llama/chat/Llama-2-7b-chat-hf/gg
             verbose=False)
 
 
-eval_dataset = json.load(open("evaluation2.json"))
+eval_dataset = json.load(open("ESConv2.json"))
+eval_ids = json.load(open("evaluation_ids.json"))
 # evaluation the model based on evaluation json
 output = [{} for _ in range(len(eval_dataset))]
 
@@ -29,7 +30,8 @@ for i in tqdm.tqdm(range(len(eval_dataset))):
     output[i]["dialog"] = [
         {
             "role": k["speaker"],
-            "content": k["content"]
+            "content": k["content"],
+            "id": k["id"],
         }
         for k in eval_dataset[i]['dialog']
     ]
@@ -37,15 +39,14 @@ for i in tqdm.tqdm(range(len(eval_dataset))):
         role = eval_dataset[i]["dialog"][j]["speaker"]
         if role == "seeker":
             message.append({"role": "user", "content": eval_dataset[i]["dialog"][j]["content"]})
-            # if len(eval_dataset[i]['dialog']) > j+1 and 'feedback' in eval_dataset[i]['dialog'][j+1]['annotation'].keys():
 
         if role == "supporter":
+            if eval_dataset[i]["dialog"][j]["id"] in eval_ids:
+                response = create_completion(message)
+                output[i]["dialog"][j]["response"] = response['choices'][0]['message']['content']
+                responses.append(response)
+                json.dump(responses, open("response.json", "w"), indent=4)
             message.append({"role": "assistant", "content": eval_dataset[i]["dialog"][j]["content"]})
-            response = create_completion(message)
-            output[i]["dialog"][j]["response"] = response['choices'][0]['message']['content']
-            responses.append(response)
-            # print(response['choices'][0]['message']['content'])
-            json.dump(responses, open("response.json", "w"), indent=4)
 
 
     # save the output to json
